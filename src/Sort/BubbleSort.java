bd6f07a6eb418510a97bdf24dd1b04a0d1f2422e
qortn4925 <qortn4925@naver.com>
2025-01-02 11:52:59 +0900

    first commit

---
+++ src/Sort/BubbleSort.java
@@ -0,0 +1,37 @@
+package Sort;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+//     int a [] = {5,3,4,1,2};
+     int a []={5,4,3,2,1};
+
+     bubbleSort(a);
+     for(int item: a){
+         System.out.println("item = " + item);
+     }
+        System.out.println("a[4]+-5 = " + a[4]+-5);
+
+    }
+
+    public static void bubbleSort(int[] arr) {
+        int count= 0;
+            //버블 소트는 ,
+            // s내림차순
+        for(int i =0; i<arr.length-1; i++){
+            for(int j = 0; j<arr.length-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int tmp =arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=tmp;
+                    count++;
+                }
+            }
+        }
+        arr[4]= arr[4]+count;
+    }
+
+    public static void upperBubbleSort(int[] arr) {
+        f
+    }
+}
